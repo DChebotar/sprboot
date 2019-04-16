16f42f3599bec0fb36153d520af3c95f6871f385
Chebotar <chebotar@tut.by>
2019-04-16 22:27:49 +0300

    start

--- src/main/java/by/dchebotar/sprboot/AuthorizationController.java
+++ src/main/java/by/dchebotar/sprboot/AuthorizationController.java
@@ -1,17 +1,28 @@
 package by.dchebotar.sprboot;
 
+import by.dchebotar.sprboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizationController {
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/auth")
-    public String auth(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model){
-        model.addAttribute("name", name);
-        return "auth";
+    @RequestMapping("/")
+    public String main(){
+        return "/main";
     }
+
+    /*@PostMapping("/login")
+    public String login(@RequestParam(name = "name", required = false, defaultValue = "") String name, Model model){
+        model.addAttribute("name", name);
+        return "/login";
+    }*/
 
 }
---
+++ src/main/java/by/dchebotar/sprboot/domain/User.java
@@ -0,0 +1,60 @@
+package by.dchebotar.sprboot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String password;
+    private String mail;
+    private Role role;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
